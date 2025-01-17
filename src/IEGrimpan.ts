cc62fd9314e1d1152ee8fb8f8c7e0b78a914873b
wlsgml0229 <wlsgml0229@naver.com>
2025-01-18 03:17:13 +0900

    ✨ 크롬그림판, IE그림판 추가

---
+++ src/IEGrimpan.ts
@@ -0,0 +1,20 @@
+class IEGrimpan {
+  private static instance: IEGrimpan;
+  constructor(canvas: HTMLElement | null) {
+    if (!canvas || !(canvas instanceof HTMLCanvasElement)) {
+      throw new Error('canvas 엘리먼트를 입력하세요');
+    }
+  }
+
+  initialize() {}
+  initializeMenu() {}
+
+  static getInstance() {
+    if (!this.instance) {
+      this.instance = new IEGrimpan(document.querySelector('canvas'));
+    }
+    return this.instance;
+  }
+}
+
+export default IEGrimpan;
