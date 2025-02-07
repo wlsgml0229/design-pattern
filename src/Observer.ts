5ef4989aea4ef071c56ca8754ef3b17fd6a767ae
wlsgml0229 <wlsgml0229@naver.com>
2025-02-08 00:54:29 +0900

    ✨ pub-sub 기능추가

--- src/Observer.ts
+++ src/Observer.ts
@@ -26,3 +26,51 @@ class SaveCompleteObserver extends Observer {
     });
   }
 }
+
+//싱글턴 패턴이면 좋다.
+//내용이 많아질거기 때문에, redis나 mysql, mongodb 같은곳에 맡길수도 있다.
+//addEvent는 그럼 해당 내용이 될 것임
+// 서버 - 오토스케일링을 많이 한다.
+//코드상 메모리에 저장하도록 해놓으면 서버들간 공유가 안됨 redis
+// 더큰 규모의 서비스 운영하기에 편리하다
+//이벤트명이 겹칮지않게 주의해주고 symbol로 사용하는걸 권장된다.
+
+export class SubscriptionManager {
+  listeners: {
+    [key: string]: Listener[];
+  } = {};
+  private static instance: SubscriptionManager;
+  private constructor() {}
+
+  addEvent(event: string) {
+    if (this.listeners[event]) {
+      return this.listeners[event];
+    }
+    this.listeners[event] = [];
+    return this.listeners[event];
+  }
+
+  subscribe(event: string, v: Listener): void {
+    this.listeners[event].push(v);
+  }
+  unsubscripbe(event: string, name: string): void {
+    this.listeners[event] = this.listeners[event].filter(
+      (v) => v.name !== name,
+    );
+  }
+
+  publish(event: string) {
+    this.listeners[event].forEach((target) => {
+      target.publish(event);
+    });
+  }
+}
+
+//메뉴
+// SubscriptionManager.getInstance().subscribe('saveComplete', {
+//   name: 'men]u',
+//   publish: this.afterComplete.bind(this),
+// });
+
+//그림판
+// SubscriptionManager.getInstance().publish('saveComplete');
